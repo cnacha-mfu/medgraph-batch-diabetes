7b67e198c0fe8c2844347b5968ef6c9d36e664ec
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2023-03-27 06:19:41 +0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker build --no-cache ./ -t us-central1-docker.pkg.dev/ailab-377802/medgraph/diabetes-batch:latest
+docker push us-central1-docker.pkg.dev/ailab-377802/medgraph/diabetes-batch:latest
